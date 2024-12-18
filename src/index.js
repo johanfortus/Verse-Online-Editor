1bb603be1771d70ed6184492918e9e30a39c60dd
johanfortus <fortusjohan@gmail.com>
2024-12-17 21:10:21 -0500

    modified index.js to initialize vs code services before rendering the app and added async function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import './verse-lsp-client.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'vscode/localExtensionHost';
+import { initialize } from 'vscode/services';
+
+(async () => {
+    await initialize();
+    // await import('./verse-lsp-client.js');
+    ReactDOM.render(<App />, document.getElementById('root'));
+})();
+
+// import './verse-lsp-client.js';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
